13d608f532402b75acf71e40ef3cbe9867c0d130
Marc Maycas <marc.maycas@gmail.com>
2016-10-30 01:37:08 +0200

    Added JSDoc documents

--- scraper.js
+++ scraper.js
@@ -3,15 +3,49 @@
 
     // Required node modules
 
+    /**
+     * Requires fs node internal module
+     * 
+     * @requires fs
+     */
     var fs = require("fs");
 
+    /**
+     * Requires osmosis module
+     * 
+     * The election of this module has been done because of:
+     *      1) Build is passing
+     *      2) Has more than 1000 downloads/month with more than 300 downloads/week (average)
+     *      3) Extensive documentation, even with a site with more detailed info
+     *      4) Easy to understand usage and comprehensive way to navigate through the links of the page by concatenating methods
+     * NPM: https://www.npmjs.com/package/osmosis
+     * Github: https://github.com/rchipka/node-osmosis
+     * @requires osmosis
+     */
     var osmosis = require("osmosis");
 
+    /**
+     * Requires json2csv module
+     * 
+     * The election of this module has been done because of:
+     *      1) Build is passing, the dependencies are up to date and has a 100% coverage 
+     *      2) More than 100,000 downloads/month with more than 6,000 downloads/day (average)
+     *      3) Extensive documentation with an extensive set of examples
+     *      4) Easy usage for the purpose of this web scraper
+     * NPM: https://www.npmjs.com/package/json2csv
+     * Github: https://github.com/zemirco/json2csv
+     * @requires json2csv
+     */
     var json2csv = require("json2csv");
 
+    // Global variable to control if an error has been fired or not when scraping, so feedback can be given in the console
+    var globalError = false;
 
-    var globalError = false; // Variable to control if an error has been fired or not when scraping, so feedback can be given in the console
-
+    /**
+     * Creates a folder in the system in case it doesn't exist
+     * 
+     * @param {string} folderName   - Name of the folder to create inside of the main app folder
+     */
     function createFolderInSystemSync(folderName) {
         if (!fs.existsSync(folderName)) {
             console.log("No /" + folderName + " folder exists");
@@ -20,6 +54,16 @@
         }
     }
 
+    /**
+     * Formats a timestamp for a specific need in the file (data to write, filename, error message) 
+     * 
+     * @param {Object} timestamp    - Date object
+     * @param {String} option       - Sets the kind of format required in the output:
+     *                                  "data" - "YYYY-MM-DD HH:MM:SS"
+     *                                  "filename" - "YYYY-MM-DD"
+     *                                  "error" - "WeekDay Month DD YYYY HH:MM:SS GMT+Ofset (Timezone)"
+     * @returns timestamp formatted for the specific purpose specified in the option
+     */
     function formatTimestamp(timestamp, option) {
         switch (option) {
             case "data":
@@ -45,10 +89,19 @@
         }
     }
 
+    /**
+     * Sets the file path of the file to write depending of the selected option
+     * 
+     * @param {String} folderName   - Name of the folder to store the file
+     * @param {String} option       - Sets the file path that will be the target file:
+     *                                  "data"  - The path to get is the 'csv' file for the current date
+     *                                  "error" - The path to get is the error-log file
+     * @returns path of the file to write, either the data file or the error-log
+     */
     function setFilePath(folderName, option) {
         var filename, path;
 
-        if (option === "data") {
+        if (option === "data" || option === undefined) {
             filename = formatTimestamp(new Date(), "filename") + ".csv";
             path = folderName + "/" + filename;
         } else if (option === "error") {
@@ -59,6 +112,12 @@
         return path;
     }
 
+    /**
+     * Writes the data to the specified file path. In case the file exists, it gets overwritten
+     * 
+     * @param {String} data         - The data to write. In this case it will be a 'csv' data
+     * @param {String} filePath     - The file path of the data file that will be written
+     */
     function writeToDataFile(data, filePath) {
         console.log("Creating file: '" + filePath + "'");
 
@@ -76,6 +135,12 @@
         });
     }
 
+    /**
+     * Writes (or in this case, appends to the end of it) the errors that have occurred during the scraping process
+     * 
+     * @param {String} data     - The data to write. In this case it will be an error string formatted accordingly
+     * @param {String} filePath - The file path of the data file that will be written
+     */
     function writeToErrorFile(data, filePath) {
         fs.appendFile(filePath, data, function (error) {
             if (error) {
@@ -84,7 +149,16 @@
         });
     }
 
-    function createFileInSystemSync(data, folderName, option) {
+    /**
+     * Writes the data in the specified file with the format specified in the option
+     * 
+     * @param {String} data         - The data to write
+     * @param {String} folderName   - Name of the folder to store the file
+     * @param {String} option       - Sets which of the files to write:
+     *                                  "data"  - Writes on the 'csv' file 
+     *                                  "error" - Writes on the error-log file
+     */
+    function writeFileInSystem(data, folderName, option) {
         if (option === "data") {
             writeToDataFile(data, setFilePath(folderName, option));
         } else if (option === "error") {
@@ -92,6 +166,11 @@
         }
     }
 
+    /**
+     * Scrapes the Shirts 4 Mike (http://www.shirts4mike.com/) website for the product list
+     * 
+     * @param {String} folderName  - Name of the folder to store the scraped data file
+     */
     function scrapeContent(folderName) {
         var jsonArray = [];
         var url = "http://www.shirts4mike.com/";
@@ -100,6 +179,7 @@
 
         createFolderInSystemSync(folderName);
 
+        // Use osmosis to go through the website and its pages
         osmosis
             .get(url)
             .follow('.nav .shirts a@href')
@@ -114,15 +194,18 @@
                 ImageURL: '.shirt-picture img@src',
             })
             .data(function (data) {
-                // Add the correct URL structure from the links
+                // As the data is generated, it gets pushed into a JSON array
+
+                // Set a URL field by prepending the hostname to it. Osmosis doesn't return the full URL but the relative path in the href attribute
                 data.URL = url + "/" + data.URL;
-                // Set the date of the specific request
+
+                // Adds a time field inside of the data Object 
                 data.Time = formatTimestamp(new Date(), "data");
 
-                // Push the data that will be converted in csv
                 jsonArray.push(data);
             })
             .done(function () {
+                // Once the scraping finishes, log the content into the file
                 if (!globalError) {
                     logContent(jsonArray, folderName);
                 } else {
@@ -130,27 +213,42 @@
                 }
             })
             .error(function (error) {
+                // If the scraping process failed, then log the error
                 logError(error, folderName);
             });
     }
 
+    /**
+     * Parses the Osmosis data into a csv file and then writes it into the file
+     * 
+     * @param {String} data         - Data to write into the file
+     * @param {String} folderName   - Name of the folder to store the scraped data file
+     */
     function logContent(data, folderName) {
         // Transform json data to csv
-        var csv = json2csv({
+        var csvData = json2csv({
             data: data,
             fields: ['Title', 'Price', 'ImageURL', 'URL', 'Time']
         });
 
-        // Write the file in the system
-        createFileInSystemSync(csv, folderName, "data");
+        writeFileInSystem(csvData, folderName, "data");
     }
 
+    /**
+     * Transforms the error info into the correctly formatted string and writes it into the file.
+     * It also sets a global error variable to true, to not log the 'csv' file
+     * 
+     * @param {String} error        - Error fired by the scraping module function
+     * @param {String} folderName   - Name of the folder to store the scraped data file
+     */
     function logError(error, folderName) {
         var errorData = "[ " + formatTimestamp(new Date(), "error") + " ] " + error + "\n";
-        globalError = true;
-        createFileInSystemSync(errorData, folderName, "error");
-    }
 
+        // Notify there's an error, so a message can be shown in the done callback 
+        globalError = true;
+
+        writeFileInSystem(errorData, folderName, "error");
+    }
 
     // Run the content-scraper
     scrapeContent("data");
