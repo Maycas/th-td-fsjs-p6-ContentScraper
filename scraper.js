73a64e6625e6d75b19ed63cc6ce8e9bb6c63ac3f
Marc Maycas <marc.maycas@gmail.com>
2016-10-29 21:48:10 +0200

    Finished implementation

--- scraper.js
+++ scraper.js
@@ -2,13 +2,15 @@
     'use strict';
 
     // Required node modules
+
     var fs = require("fs");
 
     var osmosis = require("osmosis");
 
     var json2csv = require("json2csv");
 
-    var globalError = false;
+
+    var globalError = false; // Variable to control if an error has been fired or not when scraping, so feedback can be given in the console
 
     function createFolderInSystemSync(folderName) {
         if (!fs.existsSync(folderName)) {
@@ -18,36 +20,63 @@
         }
     }
 
-    function formatTimestamp(timestamp) {
-        var day = timestamp.getDate();
-        var month = timestamp.getMonth() + 1; // January is 0, need to add 1
-        var fullYear = timestamp.getFullYear();
-
-        return fullYear + "-" + month + "-" + day;
+    function formatTimestamp(timestamp, option) {
+        switch (option) {
+            case "data":
+                // Date format of what it will be included in the 'csv' file
+                return timestamp
+                    .toISOString()
+                    .replace(/T/, ' ') // replace T with a space
+                    .replace(/\..+/, ''); // delete the dot and everything after;
+            case "filename":
+                // Date format for the 'csv' filename
+                var day = timestamp.getDate();
+                var month = timestamp.getMonth() + 1; // January is 0, need to add 1
+                var fullYear = timestamp.getFullYear();
+                return fullYear + "-" + month + "-" + day;
+            case "error":
+                // Date format to be included in the error log file
+                return timestamp.toString();
+            default:
+                // In case no option has been set, then the function logs an error asking the 
+                // developer to se an option
+                console.error("Please set an option");
+                break;
+        }
     }
 
-    function createFileInSystemSync(data, folderName) {
+    function createFileInSystemSync(data, folderName, option) {
         var today = new Date();
-        var filename = formatTimestamp(today) + ".csv";
-        var path = folderName + "/" + filename;
+        var filename, path;
 
-        createFolderInSystemSync(folderName);
+        if (option === "data") {
+            filename = formatTimestamp(today, "filename") + ".csv";
+            path = folderName + "/" + filename;
 
-        console.log("Creating file: " + filename);
+            console.log("Creating file: " + filename);
 
-        //
-        if (fs.existsSync(path)) {
-            console.log("File " + filename + " already exists in " + folderName + "/");
-            console.log("Overwriting file...");
-        }
-        fs.writeFile(path, data, function (error) {
-            if (!error) {
-                console.log("Scraping process......... END");
-            } else {
-                logError(error);
-                console.log("There was an error when writing the output file, please execute script again");
+            if (fs.existsSync(path)) {
+                console.log("File " + filename + " already exists in " + folderName + "/");
+                console.log("Overwriting file...");
             }
-        });
+            fs.writeFile(path, data, function (error) {
+                if (!error) {
+                    console.log("Scraping process......... END");
+                } else {
+                    logError(error);
+                    console.error("There was an error when writing the output file, please execute script again");
+                }
+            });
+        } else if (option === "error") {
+            filename = "scraper-error.log";
+            path = folderName + "/" + filename;
+
+            fs.appendFile(path, data, function (error) {
+                if (error) {
+                    console.error("There was an error appending the data to the file");
+                }
+            });
+        }
     }
 
     function scrapeContent(folderName) {
@@ -55,6 +84,8 @@
         var url = "http://www.shirts4mike.com/";
 
         console.log("Scraping process......... START");
+
+        createFolderInSystemSync(folderName);
 
         osmosis
             .get(url)
@@ -73,7 +104,8 @@
                 // Add the correct URL structure from the links
                 data.URL = url + "/" + data.URL;
                 // Set the date of the specific request
-                data.Time = new Date();
+                data.Time = formatTimestamp(new Date(), "data");
+
                 // Push the data that will be converted in csv
                 jsonArray.push(data);
             })
@@ -85,7 +117,7 @@
                 }
             })
             .error(function (error) {
-                logError(error);
+                logError(error, folderName);
             });
     }
 
@@ -97,13 +129,13 @@
         });
 
         // Write the file in the system
-        createFileInSystemSync(csv, folderName);
+        createFileInSystemSync(csv, folderName, "data");
     }
 
-    function logError(error) {
-        // TODO: Log the error in a file
+    function logError(error, folderName) {
+        var errorData = "[ " + formatTimestamp(new Date(), "error") + " ] " + error + "\n";
         globalError = true;
-        console.log(error);
+        createFileInSystemSync(errorData, folderName, "error");
     }
 
     // Run the content-scraper
